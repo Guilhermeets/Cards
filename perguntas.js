b34f6128f0d4d8c2839cae6f6554216ea8c248a5
Guilhermeets <1guilhermeets1@gmail.com>
2024-11-29 00:21:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python usado para fazer?',
+    'O Python é usado para desenvolvimento web, ciência de dados, automação e mais'
+);
+
+criaCartao(
+    'Geografia',
+    'Qual o país mais populoso?',
+    'O país mais populoso é a China'
+);
+
+criaCartao(
+    'Programação',
+    'Por que usar algoritmos?',
+    'Algoritmos ajudam a resolver problemas de forma lógica e eficiente'
+);
+
+criaCartao(
+    'Língua inglesa',
+    'Como perguntar “como vai você”?',
+    'Em inglês, diz-se “How are you?” (Rau ár iu)'
+);
